5bb9afcbfe6e6d3b7248d4d677e36bbc81e59e97
realmadrooster <119696582+realmadrooster@users.noreply.github.com>
2022-12-23 22:42:00 -0300

    23-12-22/2

---
+++ Qual é seu time?/assets/main.js
@@ -0,0 +1,11 @@
+
+
+function calculaTime(){
+    
+    const perguntaUm = document.getElementsByName('p1');
+    if (perguntaUm[1].checked){
+        alert('galoo');
+    }else{
+        alert('outro');
+    }
+}
