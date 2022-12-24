e0dd78aba5da493bba7efa51fe215a0ae4087006
realmadrooster <119696582+realmadrooster@users.noreply.github.com>
2022-12-24 14:40:24 -0300

    24-12-22

--- Qual é seu time?/assets/main.js
+++ Qual é seu time?/assets/main.js
@@ -1,11 +1,22 @@
 
 
+
+//img.setAttribute('id','foto');
 function calculaTime(){
+    const trocaFoto = document.querySelector('.fotopadrao')
+    const fotoTime = document.querySelector('.fotoTime');
+    var img=document.createElement('img');
     
     const perguntaUm = document.getElementsByName('p1');
     if (perguntaUm[1].checked){
         alert('galoo');
+        trocaFoto.setAttribute('src','assets/imagens/galotime.jpg')
+        img.setAttribute('src','assets/imagens/galo.jpg');
+        
     }else{
         alert('outro');
     }
+    
+    fotoTime.style.textAlign='center'
+    fotoTime.appendChild(img);
 }
